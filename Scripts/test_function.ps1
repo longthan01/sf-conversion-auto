b8783b8fb48b05ac120c8b3363bc49c550c5230c
Hung Vo <Hung.Khanh.Vo@steadfasttech.com.au>
2019-01-16 22:30:31 +0700

    update xxx2

--- Scripts/test_function.ps1
+++ Scripts/test_function.ps1
@@ -4,6 +4,7 @@ function main () {
     Write-Output "begin";
 
     $PreConvRecordCountContent = Get-Content -Path "F:\Steadfast\Migration\PreConversionRecordCount.txt"
+    $PostConvRecordCountContent = Get-Content -Path "F:\Steadfast\Migration\PostConversionRecordCount.txt"
     $start_table = 0
     $end_table = 20
     $regex = "[-][-][-][-][-][-][-][-][-][-][-][-][-][-][-][-][-]"
@@ -18,84 +19,79 @@ function main () {
 
 
     #Loop pre conversion table
+    Write-Output "/******************** Insert to pre conversion table ***************/"
     $intRow = 1
 
     Do {
         
         $ColumnID = $UserWorksheet.Cells.Item($intRow, 1).Value()
         Write-Output "-------------------$ColumnID-----------------"
-        # If ($ColumnID -gt 0) {
-        #     ForEach ($record in $recordSet) {
-        #         #for each address found, concatinate them into one variable
-        #         Write-Output $record
-        #     }
-        # }
-        UpdateFields "Entities" "*Entities*entities*" $UserWorksheet
-        UpdateFields "Profiles" "*Profiles*profile*" $UserWorksheet
-        UpdateFields "Contacts" "*Contacts*personnel*" $UserWorksheet
-        UpdateFields "Addresseses" "*Addresses*addresses*" $UserWorksheet
-        UpdateFields "Authorised Reps" "*Authorised Reps*entities*" $UserWorksheet
-        UpdateFields "All Insurers" "*All Insurers*entities*" $UserWorksheet
-        UpdateFields "SVU Insurers" "*SVU Insurers*N/A (table does not exist)*" $UserWorksheet
-        UpdateFields "SVU Products" "*SVU Products*" $UserWorksheet
-        UpdateFields "Sunrise Insurers" "*Sunrise Insurers*sunrise_insurers*" $UserWorksheet
-        UpdateFields "Sunrise Products" "*Sunrise Products*sunrise_products*" $UserWorksheet
-        UpdateFields "Client Tasks" "*Client Tasks*tasks*" $UserWorksheet
-        UpdateFields "Client Taks Documents" "*Client Taks Documents*N/A (table does not exist)*" $UserWorksheet
-        UpdateFields "Policy Tasks" "*Policy Tasks*journals*" $UserWorksheet
-        UpdateFields "Policy Taks Documents" "*Policy Taks Documents*N/A (table does not exist)*" $UserWorksheet
-        UpdateFields "Claims" "*Claims*claims*" $UserWorksheet
-        UpdateFields "Claim Tasks" "*Claim Tasks*claims*" $UserWorksheet
-        UpdateFields "Claim Taks Documents" "*Claim Taks Documents*N/A (table does not exist)*" $UserWorksheet
-        UpdateFields "Policies" "*Policies*policies*" $UserWorksheet
-        UpdateFields "Invoices" "*Invoices*policies*" $UserWorksheet
-        UpdateFields "Sunrise Policies" "*Sunrise Policies*N/A (table does not exist)*" $UserWorksheet
-        UpdateFields "SVU Policies" "*SVU Policies*N/A (table does not exist)*" $UserWorksheet
-        UpdateFields "SVU Policy Opportunities" "*SVU Policy Opportunities*SVUPolicies*" $UserWorksheet
+        UpdateFields "Entities" "*Entities*entities*" $UserWorksheet "pre"
+        UpdateFields "Profiles" "*Profiles*profile*" $UserWorksheet "pre"
+        UpdateFields "Contacts" "*Contacts*personnel*" $UserWorksheet "pre"
+        UpdateFields "Addresseses" "*Addresses*addresses*" $UserWorksheet "pre"
+        UpdateFields "Authorised Reps" "*Authorised Reps*entities*" $UserWorksheet "pre"
+        UpdateFields "All Insurers" "*All Insurers*entities*" $UserWorksheet "pre"
+        UpdateFields "SVU Insurers" "*SVU Insurers*N/A (table does not exist)*" $UserWorksheet "pre"
+        UpdateFields "SVU Products" "*SVU Products*" $UserWorksheet "pre"
+        UpdateFields "Sunrise Insurers" "*Sunrise Insurers*sunrise_insurers*" $UserWorksheet "pre"
+        UpdateFields "Sunrise Products" "*Sunrise Products*sunrise_products*" $UserWorksheet "pre"
+        UpdateFields "Client Tasks" "*Client Tasks*tasks*" $UserWorksheet "pre"
+        UpdateFields "Client Taks Documents" "*Client Taks Documents*N/A (table does not exist)*" $UserWorksheet "pre"
+        UpdateFields "Policy Tasks" "*Policy Tasks*journals*" $UserWorksheet "pre"
+        UpdateFields "Policy Taks Documents" "*Policy Taks Documents*N/A (table does not exist)*" $UserWorksheet "pre"
+        UpdateFields "Claims" "*Claims*claims*" $UserWorksheet "pre"
+        UpdateFields "Claim Tasks" "*Claim Tasks*claims*" $UserWorksheet "pre"
+        UpdateFields "Claim Taks Documents" "*Claim Taks Documents*N/A (table does not exist)*" $UserWorksheet "pre"
+        UpdateFields "Policies" "*Policies*policies*" $UserWorksheet "pre"
+        UpdateFields "Invoices" "*Invoices*policies*" $UserWorksheet "pre"
+        UpdateFields "Sunrise Policies" "*Sunrise Policies*N/A (table does not exist)*" $UserWorksheet "pre"
+        UpdateFields "SVU Policies" "*SVU Policies*N/A (table does not exist)*" $UserWorksheet "pre"
+        UpdateFields "SVU Policy Opportunities" "*SVU Policy Opportunities*SVUPolicies*" $UserWorksheet "pre"
 
         $intRow++
     } While ($ColumnID)
 
     # Loop for post conversion table
-
-    # $intRow = 27
-    # Do {
+    Write-Output "/******************** Insert to post conversion table ***************/"
+    $intRow = 27
+    Do {
         
-    #     $ColumnID = $UserWorksheet.Cells.Item($intRow, 1).Value()
-    #     Write-Output $ColumnID
+        $ColumnID = $UserWorksheet.Cells.Item($intRow, 1).Value()
+        Write-Output "-------------------$ColumnID-----------------"
  
-    #     UpdateFields "Entities" "*Entities*entities*" UserWorksheet
-    #     UpdateFields "Profiles" "*Profiles*profile*" UserWorksheet
-    #     UpdateFields "Contacts" "*Contacts*personnel*" UserWorksheet
-    #     UpdateFields "Addresseses" "*Addresseses*addresses*" UserWorksheet
-    #     UpdateFields "Authorised Reps" "*AuthorisedReps*entities*" UserWorksheet
-    #     UpdateFields "All Insurers" "*AllInsurers*entities*" UserWorksheet
-    #     UpdateFields "SVU Insurers" "*SVUInsurers*entities*" UserWorksheet
-    #     UpdateFields "SVU Products" "*SVUProducts*CustomWorkbooks*" UserWorksheet
-    #     UpdateFields "Sunrise Insurers" "*SunriseInsurers*sunrise_insurers*" UserWorksheet
-    #     UpdateFields "Sunrise Products" "*SunriseProducts*sunrise_products*" UserWorksheet
-    #     UpdateFields "Client Tasks" "*ClientTasks*tasks*" UserWorksheet
-    #     UpdateFields "Client Taks Documents" "*ClientTaksDocuments*tasks&tasks_sub_tasks*" UserWorksheet
-    #     UpdateFields "Policy Tasks" "*PolicyTasks*journals*" UserWorksheet
-    #     UpdateFields "Policy Taks Documents" "*PolicyTaksDocuments*journals&journal_sub_tasks*" UserWorksheet
-    #     UpdateFields "Claims" "*Claims*claims*" UserWorksheet
-    #     UpdateFields "Claim Tasks" "*ClaimTasks*claims*" UserWorksheet
-    #     UpdateFields "Claim Taks Documents" "*ClaimTaksDocuments*clatas_id&clasubta_id*" UserWorksheet
-    #     UpdateFields "Policies" "*Policies*pol_id*" UserWorksheet
-    #     UpdateFields "Invoices" "*Invoices*tran_id*" UserWorksheet
-    #     UpdateFields "Sunrise Policies" "*SunrisePolicies*sunwor_id*" UserWorksheet
-    #     UpdateFields "SVU Policies" "*SVUPolicies*wor_id*" UserWorksheet
-    #     UpdateFields "SVU Policy Opportunities" "*SVUPolicyOpportunities*WorkbookHeader*" UserWorksheet
+        UpdateFields "Entities" "*Entities*entities*" $UserWorksheet "post"
+        UpdateFields "Profiles" "*Profiles*profile*" $UserWorksheet "post"
+        UpdateFields "Contacts" "*Contacts*personnel*" $UserWorksheet "post"
+        UpdateFields "Addresseses" "*Addresses*addresses*" $UserWorksheet "post"
+        UpdateFields "Authorised Reps" "*Authorised Reps*entities*" $UserWorksheet "post"
+        UpdateFields "All Insurers" "*All Insurers*entities*" $UserWorksheet "post"
+        UpdateFields "SVU Insurers" "*SVU Insurers*svu_insurers*" $UserWorksheet "post"
+        UpdateFields "SVU Products" "*SVU Products*svu_products*" $UserWorksheet "post"
+        UpdateFields "Sunrise Insurers" "*Sunrise Insurers*sunrise_insurers*" $UserWorksheet "post"
+        UpdateFields "Sunrise Products" "*Sunrise Products*sunrise_products*" $UserWorksheet "post"
+        UpdateFields "Client Tasks" "*Client Tasks*tasks*" $UserWorksheet "post"
+        UpdateFields "Client Taks Documents" "*Client Taks Documents*tasks_sub_tasks*" $UserWorksheet "post"
+        UpdateFields "Policy Tasks" "*Policy Tasks*journals*" $UserWorksheet "post"
+        UpdateFields "Policy Taks Documents" "*Policy Taks Documents*journal_sub_tasks*" $UserWorksheet "post"
+        UpdateFields "Claims" "*Claims*claims*" $UserWorksheet "post"
+        UpdateFields "Claim Tasks" "*Claim Tasks*claims*" $UserWorksheet "post"
+        UpdateFields "Claim Taks Documents" "*Claim Taks Documents*tasks_sub_tasks*" $UserWorksheet "post"
+        UpdateFields "Policies" "*Policies*policies*" $UserWorksheet "post"
+        UpdateFields "Invoices" "*Invoices*policies*" $UserWorksheet "post"
+        UpdateFields "Sunrise Policies" "*Sunrise Policies*sunrise_policies*" $UserWorksheet "post"
+        UpdateFields "SVU Policies" "*SVU Policies*SVUPolicies*" $UserWorksheet "post"
+        UpdateFields "SVU Policy Opportunities" "*SVU Policy Opportunities*SVUPolicies*" $UserWorksheet "post"
 
      
-    #     $intRow++
-    # } While ($ColumnID)
+        $intRow++
+    } While ($ColumnID)
 
 
     Write-Output "end"
     Write-Output  $intRow
 }
-function UpdateFields ([String]$excelPattern, [String] $textPattern, $worksheet) {
+function UpdateFields ([String]$excelPattern, [String] $textPattern, $worksheet, [String] $type = "pre") {
     If ($ColumnID -contains $excelPattern) {
         # $distinct_count = $worksheet.Cells.Item($intRow, 4).Value()
         # $min_value = $worksheet.Cells.Item($intRow, 5).Value()
@@ -103,28 +99,64 @@ function UpdateFields ([String]$excelPattern, [String] $textPattern, $worksheet)
         # Write-Output $distinct_count
         # Write-Output $max_value
         # Write-Output $min_value
-        foreach ($line in $PreConvRecordCountContent) {
-            if ($line -like $textPattern) {
-                Write-Output "line $count $line"
-                $splitUp = $line.substring(300) -split "\s+"
-                if ($splitUp) {
-                    # distinct count
-                    Write-Output "distinct count" $splitUp[1]
-                    $worksheet.Cells.Item($intRow, 4) = $splitUp[1]
-                    # min value
-                    Write-Output "min value" $splitUp[1]
-                    $worksheet.Cells.Item($intRow, 5) = $splitUp[2]
-                    # max value
-                    Write-Output "max value" $splitUp[1]
-                    $worksheet.Cells.Item($intRow, 6) = $splitUp[3]
-                } else {
-                    Write-Output "No line match"
+        If($type -eq "pre"){
+            foreach ($line in $PreConvRecordCountContent) {
+                if ($line -like $textPattern) {
+                    Write-Output "line $count $line"
+                    $splitUp = $line.substring(300) -split "\s+"
+                    if ($splitUp) {
+                        # distinct count
+                        Write-Output "distinct count" $splitUp[1]
+                        $worksheet.Cells.Item($intRow, 4) = $splitUp[1]
+                        # min value
+                        Write-Output "min value" $splitUp[1]
+                        $worksheet.Cells.Item($intRow, 5) = $splitUp[2]
+                        # max value
+                        Write-Output "max value" $splitUp[1]
+                        $worksheet.Cells.Item($intRow, 6) = $splitUp[3]
+                    } else {
+                        Write-Output "No line match"
+                    }
                 }
+               
+                $count++
             }
-           
-
-            $count++
+        } else{
+            foreach ($line in $PostConvRecordCountContent) {
+                if ($line -like $textPattern) {
+                    Write-Output "line $count $line"
+                    $splitUp = $line.substring(300) -split "\s+"
+                    if ($splitUp) {
+                        # distinct count
+                        Write-Output "distinct count" $splitUp[1]
+                        $worksheet.Cells.Item($intRow, 4) = $splitUp[1]
+                        
+                        if ($line -like "*N/A (different table used)*") {
+                            Write-Output "min max value N/A (different table used) " 
+                            $worksheet.Cells.Item($intRow, 5)  = "N/A (different table used)"
+                            $worksheet.Cells.Item($intRow, 6) = "N/A (different table used)"
+                        } elseif ($line -like "*N/A (different logic used)*") {
+                            Write-Output "min max value N/A (different logic used) " 
+                            $worksheet.Cells.Item($intRow, 5)  = "N/A (different logic used)"
+                            $worksheet.Cells.Item($intRow, 6) = "N/A (different logic used)"
+                        } else {
+                            # min value
+                            Write-Output "min value" $splitUp[2]
+                            $worksheet.Cells.Item($intRow, 5) = $splitUp[2]
+                            # max value
+                            Write-Output "max value" $splitUp[3]
+                            $worksheet.Cells.Item($intRow, 6) = $splitUp[3]
+                        }
+                        
+                    } else {
+                        Write-Output "No line match"
+                    }
+                }
+               
+                $count++
+            }
         }
+        
     }
 }
 main  
